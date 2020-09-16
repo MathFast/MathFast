a4de0d805731f55fd668bee098e9bd6095ed1602
Joonas Laakkonen <61282707+LaakJoonas@users.noreply.github.com>
2020-09-16 14:00:47 +0300

    ctrl s
    xd

--- src/mathfast/UIManager.java
+++ src/mathfast/UIManager.java
@@ -20,7 +20,7 @@ public class UIManager extends JFrame{
     }
     protected void initWindow(){
         setTitle(Flags.app_name + " v." + Flags.app_ver);
-        setSize(888, 500);
+        setSize(888, 350);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
         setVisible(true);
